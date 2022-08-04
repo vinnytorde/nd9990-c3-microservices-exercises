e069c7b60cbb756e383fce5a3f46b8fae8e2857d
vinny <vinnytorde@gmail.com>
2022-08-03 21:51:52 -0500

    separate monolith into microservices

--- project/set_env.sh
+++ project/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=password1
-export POSTGRES_HOST=udagram2.csixzufepzrt.us-west-2.rds.amazonaws.com
-export POSTGRES_DB=udagram
-export AWS_BUCKET=arn:aws:s3:::udaconnect
-export AWS_REGION=us-west-2
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-1.cnhji4lbydmh.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::nanodegree-project-3
+export AWS_REGION=us-east-1
 export AWS_PROFILE=test
 export JWT_SECRET=testing
 export URL=http://localhost:8100
